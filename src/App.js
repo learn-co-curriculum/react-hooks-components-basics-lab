b5f2fd7050d16d7da1f4654c4c31a3282b136bf9
Maxwell Benton <maxwellbenton@gmail.com>
2019-01-14 11:19:34 -0500

    reverts starter code

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ class App extends Component {
 		// your code in the return statement below!
 		return (
 			<div className="App">
-				<CatComponent />
-				<EinsteinQuoteComponent />
-				<MouseComponent />
 			</div>
 		);
 	}
