21d8b40a4d8977fe7c066bd059e8de814dcced6d
maxwellbenton <maxwellbenton@gmail.com>
2019-10-07 17:20:53 -0400

    adjusts starter code to align with instructions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import CatComponent from './CatComponent.js';
-import { EinsteinQuoteComponent } from './EinsteinQuoteComponent';
-import MouseComponent from './MouseComponent';
 
 class App extends Component {
 	render() {
 		// your code in the return statement below!
 		return (
 			<div className="App">
+				<CatComponent />
+				<EinsteinQuoteComponent />
+				<MouseComponent />
 			</div>
 		);
 	}
