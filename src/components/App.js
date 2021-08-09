d1a95166f7ba2b032128ef98eedc8b4515122758
Liz Burton <liz.burton147@gmail.com>
2021-08-09 11:55:20 -0400

    Update starter code for clarity

--- src/components/App.js
+++ src/components/App.js
@@ -11,17 +11,20 @@ function NavBar() {
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h2>Home</h2>
     </div>
   );
 }
+
+{/* write an <About> component here */}
+
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      {/* add your <About> component here */}
     </div>
   );
 }
