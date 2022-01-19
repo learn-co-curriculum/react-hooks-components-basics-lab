f87fa64511836f0475959f59563cbf8c9ed0365f
Ian Hollander <ianholla@gmail.com>
2022-01-19 07:31:17 -0500

    Add hints

--- README.md
+++ README.md
@@ -46,6 +46,14 @@ to check your work.
     match the screenshot, the div should contain an `<h2>` element with the text
     "About." However, as long as it has an id of `about`, it will pass the test!
 
+**Hint**: If you're stuck, try using the `screen.debug` method in the test file
+to see what DOM elements are being returned by your `<App>` component. If you
+still need help, this lab (and others) have a [solution branch][] on GitHub
+where you can see one possible solution to this lesson.
+
 ## Resources
 
 - [React Components](https://reactjs.org/docs/components-and-props.html)
+
+[solution branch]:
+  https://github.com/learn-co-curriculum/react-hooks-running-tests/tree/solution
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import App from "../components/App";
 
@@ -9,6 +9,13 @@ test("renders without errors", () => {
 
 test("renders the correct child components", () => {
   const { container } = render(<App />);
+
+  /*
+    Uncomment the line below to see the DOM elements being returned
+    by the App component in your terminal when you run the tests
+  */
+  // screen.debug();
+
   expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
   expect(container.querySelector("#about")).toBeInTheDocument();
