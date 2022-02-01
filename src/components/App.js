5da17cb5eb801956e10a3d6b6b88105e571e5c32
Evan Beck <evandrydenbeck@gmail.com>
2022-02-01 16:45:34 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
