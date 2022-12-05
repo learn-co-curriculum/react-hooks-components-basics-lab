2c0478e5e74c460614a3a45de061e66ac875f237
Hadi <hadi.maleh@gmail.com>
2022-12-05 16:57:35 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -11,20 +11,24 @@ function NavBar() {
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h2>Home</h2>
     </div>
   );
 }
-
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
