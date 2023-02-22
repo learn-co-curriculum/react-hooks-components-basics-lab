79e43c2cd4d2cb2c29c62a724e785fafee702cee
David Doan <daviddoan@Davids-MacBook-Pro.local>
2023-02-22 12:30:09 -0700

    revert to last commit

--- src/components/App.js
+++ src/components/App.js
@@ -17,20 +17,12 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-function About() {
-  return(
-    <div id = "about">
-      <h2>About</h2>
-    </div>
-  )
-}
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home/>
-      <About/>
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
