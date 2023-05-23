8ca54a092500c5758e22ca2a72e193b7f83485aa
Oli Treadwell <oliver.treadwell@gmail.com>
2023-05-23 14:11:32 -0700

    refactor and separate tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -7,7 +7,7 @@ test("renders without errors", () => {
   expect(() => render(<App />)).not.toThrow();
 });
 
-test("renders the correct child components", () => {
+test("renders the child component <Home />", () => {
   const { container } = render(<App />);
 
   /*
@@ -16,7 +16,33 @@ test("renders the correct child components", () => {
   */
   // screen.debug();
 
-  expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
+});
+
+test("renders the child component <About />", () => {
+  const { container } = render(<App />);
+
+  /*
+    Uncomment the line below to see the DOM elements being returned
+    by the App component in your terminal when you run the tests
+  */
+  // screen.debug();
+
   expect(container.querySelector("#about")).toBeInTheDocument();
 });
+
+test("renders the child component <About /> with id='about'", () => {
+  const { container } = render(<App />);
+
+  expect(container.querySelector("#about")).toHaveAttribute('id', 'about')
+});
+
+
+test("renders <About /> component with child h2 HTML element", () => {
+  const { container } = render(<App />);
+
+  const About = container.querySelector("#about")
+
+  expect(About.querySelector("h2")).toBeInTheDocument();
+  expect(About.querySelector("h2")).toHaveTextContent("About")
+})
