d0ad28f11554a0f01ad75d68a590675e1ddf3381
Hunç Taşçı <51780883+hunctasci@users.noreply.github.com>
2023-06-20 11:43:05 +0000

    Completed assignemnt

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,15 @@ function Home() {
 
 {/* write an <About> component here */}
 
+const About = () => <div id='about'><h2>About</h2></div>
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>     
     </div>
   );
 }
